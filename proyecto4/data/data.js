1c2b627628e036f783bdd7b5a5a3a7abd0526822
oscarzafra <oscarzafra35@gmail.com>
2024-12-04 16:19:49 +0100

    Completo el Data

---
+++ proyecto4/data/data.js
@@ -0,0 +1,84 @@
+const data = {
+    name: "Óscar",
+    surname: "Zafra",
+    email: "oscarzafra35@gmail.com",
+    photo: "",
+    about_me: "Estudiante de informática, caracterizado por ser una persona con talante asertivo, empático y solidario.Comprometido a crecer en el ámbito laboral, con iniciativa para adquirir experiencia que me permita mejorar de cara a un futuro mi carrera como profesional.",
+
+    experience: {
+        third_job: [
+            {
+                postition: "Técnico Especialista en Radioterapia y Dosimetría",
+                company: "Hospital de la Marimorena",
+                antique: "3 meses",
+                about_job: "Tratamientos de radioterapia con aceleradores lineales, cont técnicas de VMAT y RT3D. Además, realización de TAC en pacientes en su pre-tratamiento.",
+
+            }
+        ],
+
+        second_job: [
+            {
+                postition: "Operador de Producción",
+                company: "Fábrica de Fruta Fría Frigorizada",
+                antique: "1 meses",
+                about_job: "Formación y elaboración del producto mediante procesos de congelación.",
+            }
+        ],
+
+        first_job: [
+            {
+                postition: "Auxiliar de Restauración",
+                company: "Parque Warner",
+                antique: "2 meses",
+                about_job: "Atención al cliente, cobro con TPV y caja, limpieza y reposición de productos, camarero en buffet y venta en puesto ambulante.",
+            }
+        ],
+
+    },
+
+    education: {
+        
+        third_title: [
+            {
+                title: "Técnico Superior en Desarrollo de Aplicaciones Multiplataforma.",
+                school: "thePower",
+                start: "09/2024",
+                end: "Actual",
+
+            }
+        ],
+
+        second_title: [
+            {
+                title: "Máster en Desarrollo de Aplicaciones Web",
+                school: "thePower",
+                start: "02/2024",
+                end: "Actual",
+
+            }
+        ],
+
+        first_title: [
+            {
+                title: "Técnico Superior en Radioterapia y Dosimetría",
+                school: "ITEP",
+                start: "09/2021",
+                end: "06/2023",
+
+            }
+        ],
+    },
+
+    licenses: {
+        first_license: [
+            {
+                title: "Operador en Instalaciones Radiactivas",
+                school: "CSN",
+                expedition: "2023",
+            }
+        ],
+    },
+
+}
+
+export default data;
